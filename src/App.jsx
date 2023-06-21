f89d6c9fc5b217c943c4046727595f20f1b37337
Lajisha <Lajishab8631@gmail.com>
2023-06-21 10:45:38 +0530

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Routes>
+        <Route path="/"  element={<Home />} /> 
+        <Route path="/Register"  element={<Register/>} /> 
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
